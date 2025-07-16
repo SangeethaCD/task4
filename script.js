9d5075dcf6ff9131ea43dbcc1e597ab3e8180e5a
sangeetha <sangeetha.sakthimurugan@crystaldelta.com>
2025-07-16 13:44:47 +0530

    modification

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function myFunction()
-{
-    const firstname=document.getElementById("firstname");
-    const secondname=document.getElementById("secondname");
-    const number=document.getElementById("contactNumber");
-    const email = document.getElementById("email");
-    const location = document.getElementById("number");
+function myFunction() {
+    const firstname = document.getElementById("firstname").value.trim();
+    const secondname = document.getElementById("secondname").value.trim();
+    const number = document.getElementById("contactNumber").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const location = document.getElementById("location").value.trim();
 
-    if(!firstname||!secondname||!number||!email||!location)
-    {
-        document.getElementById("error-messages").style.display="block";
+    if (firstname === "" || secondname === "" || number === "" || email === "" || location === "") {
+        document.getElementById("error-messages").style.display = "block";
+    } else {
+        document.getElementById("error-messages").style.display = "none";
+        alert("Thef from is submitted");
     }
-
 }
